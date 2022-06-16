961040014f9d2811fe204b6981af4d5f5d335f72
rebecca <yingyxu@outlook.com>
2022-06-16 12:00:06 -0500

    drop the Invalid in San Antonio

--- datacollectionapp.py
+++ datacollectionapp.py
@@ -1,10 +1,9 @@
 
-#API Key ID
-#9yz7a9cjftdr9qbvdlhz4ii9l
+# API Key ID
+# 9yz7a9cjftdr9qbvdlhz4ii9l
 
-#API Key Secret
-#e48r6otlbnf9bt6qd6ymci5hudrbwbk582ulbccarit04orlr
-
+# API Key Secret
+# e48r6otlbnf9bt6qd6ymci5hudrbwbk582ulbccarit04orlr
 
 
 from telnetlib import AUTHENTICATION
@@ -17,19 +16,19 @@ import urllib3
 import json
 
 
-def search(a,b,c):
-    
+def search(a, b, c):
+
     print("Input fields or NA")
     print("If searching a state government type state name in city and state feilds")
     city = a
     state = b
     topic_name = c
-    
-    city_api_list = pd.read_csv("city_api_list.csv", index_col = False)
-        
-    #if city_api_list.loc[city_api_list['City'].str.contains(city)] != None:
+
+    city_api_list = pd.read_csv("city_api_list.csv", index_col=False)
+
+    # if city_api_list.loc[city_api_list['City'].str.contains(city)] != None:
     if city_api_list["City"].eq(city).any() and city_api_list["State-Abbr."].eq(state).any():
-        #print('True')
+        # print('True')
 
         # x = city index
         x = city_api_list.loc[city_api_list['City'].str.contains(city)]
@@ -45,62 +44,70 @@ def search(a,b,c):
         var = var + '&q=' + str(topic_name)
         print(var)
 
-    
     else:
         print("City Not Found.")
         var = ''
     return var
 
 
+def printData(data):
+    print("THIS IS TESTING DATASET -----------------------------")
+    print(data)
+    print("END -------------------------------------")
 
 
-def mainprogram(a,b,c):
-    
-    
+def mainprogram(a, b, c):
+
     stoprg = 0
-    
-    city_domain = str(search(a,b,c))
+
+    city_domain = str(search(a, b, c))
     if city_domain == '':
         # empty_df = pd.DataFrame(None, None)
         # return empty_df
         return None
     http = urllib3.PoolManager()
 
-    #?domains=data.austintexas.gov'
-   
     # request_site = 'https://api.us.socrata.com/api/catalog/v1'+ city_domain
-    request_site = 'https://phoenixopendata.com/api/3/action/package_search?q=transport'
+    # request_site = 'https://data.sanantonio.gov/api/3/action/package_search?q=health'
+    request_site = 'https://phoenixopendata.com/api/3/action/package_search?q=health'
     # request_site = 'https://ridb.recreation.gov/api/v1/'
-    request = http.request('GET',request_site)
+    request = http.request('GET', request_site)
 
     #response_body = urlopen(request).read()
     data = json.loads(request.data)
     # results_df = pd.json_normalize(data['results'])
 
-    while True: 
+    while True:
         results_df = pd.json_normalize(data['result'], record_path=['results'])
         if results_df.size == 0:
             return results_df
         break
-    print("this is dataset -----------------------------")
-    print(results_df.to_string())       
-    #DataFrame.from_records(str(request.data))
-    print("end-------------------------------------")
-    results_df['Index'] = range(1,len(results_df)+1)
-    #print(results_df.head())
+    printData(results_df.to_string())
+    results_df['Index'] = range(1, len(results_df)+1)
+    # print(results_df.head())
     results_df.set_index('Index')
-    #['resource.name']
-    a = pd.DataFrame(data, columns = ['Index', 'Name'], index = None)
+    # ['resource.name']
+    a = pd.DataFrame(data, columns=['Index', 'Name'], index=None)
     a['Index'] = results_df['Index']
+    # SCORATA
+    #   Final displayed data frame page NAME & More Info
     # a['Name'] = results_df['resource.name']
     # a['More Info'] = results_df['permalink']
+
+    # CKAN
+    #   Final displayed data frame Name & More Infor
     a['Name'] = results_df['title']
     a['More Info'] = results_df['url']
-    #print(a[['Index','Name']].to_string(index=False))
+    print(a.to_string())
+    # Drop the cell doesn't contain https
+    httpString = "https://"
+    #a = a[a['More Info'].str.contains(httpString) == True]
+    # After drop all invalid data, assign the Index
+    a['Index'] = range(1, len(a)+1)
     return a
-    #results_df.to_csv('results_test.csv')
+    # results_df.to_csv('results_test.csv')
     # print()
-    
+
     '''
     #download the datasets process into a csv (need to move to a function)
     print("Select datasets seperated by commas", end = "")
@@ -126,9 +133,9 @@ def mainprogram(a,b,c):
             final.to_csv(y)
             print("Downloaded", y)  
     
-     '''   
+     '''
+
+
 def downloaddata(a):
-    print('test')         
-    #print(request.data)
-
-
+    print('test')
+    # print(request.data)
