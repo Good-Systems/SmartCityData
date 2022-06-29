f80b623c5a4aaa5ae28735fa9f0236112a4ce1da
Pandora727 <fanziyue2022@outlook.com>
2022-06-29 00:57:38 -0500

    gabby csv file updated for all city api

--- CityUpdate.py
+++ CityUpdate.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
-f = open('CitiesUpdate.txt', "w+")
+f = open('indexUpdate.txt', "w+")
 citycsv = pd.read_csv('city_api_list.csv')
 # print(citycsv['City'][0])
-counter = 1
-for i in range(citycsv.shape[0]):
-    f.write('INSERT INTO city (id, state, name)')
+counter = 61
+for i in range(57, 106):
+    f.write('UPDATE city')
     f.write('\n')
-    f.write('VALUES ('+ str(counter) +', \''+ citycsv['State-Abbr.'][i] + '\', \'' + citycsv['City'][i] + '\');')
+    f.write('SET id=' + str(counter))
+    f.write('\n')
+    f.write('WHERE name = \'' + citycsv['City'][i-1] + '\' & state = \'' + citycsv['State-Abbr.'][i-1] + '\';')
+    # f.write('VALUES ('+ str(counter) +', \''+ citycsv['State-Abbr.'][i] + '\', \'' + citycsv['City'][i] + '\');')
     f.write('\n')
     f.write('\n')
     counter += 1
